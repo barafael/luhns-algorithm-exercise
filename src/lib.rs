dd814229dd7683efb4e9e3def46563109259ef37
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2023-10-03 20:35:41 +0200

    Ignore unit tests - they are to be un-ignored

--- src/lib.rs
+++ src/lib.rs
@@ -5,41 +5,55 @@ pub fn luhn(cc_number: &str) -> bool {
     todo!()
 }
 
-#[test]
-fn test_non_digit_cc_number() {
-    assert!(!luhn("foo"));
-}
+#[cfg(test)]
+mod test {
+    use super::*;
 
-#[test]
-fn test_empty_cc_number() {
-    assert!(!luhn(""));
-    assert!(!luhn(" "));
-    assert!(!luhn("  "));
-    assert!(!luhn("    "));
-}
+    // TODO un-ignore the tests
+    // then make them pass!
 
-#[test]
-fn test_single_digit_cc_number() {
-    assert!(!luhn("0"));
-}
+    #[test]
+    #[ignore]
+    fn test_non_digit_cc_number() {
+        assert!(!luhn("foo"));
+    }
 
-#[test]
-fn test_two_digit_cc_number() {
-    assert!(luhn(" 0 0 "));
-}
+    #[test]
+    #[ignore]
+    fn test_empty_cc_number() {
+        assert!(!luhn(""));
+        assert!(!luhn(" "));
+        assert!(!luhn("  "));
+        assert!(!luhn("    "));
+    }
 
-#[test]
-fn test_valid_cc_number() {
-    assert!(luhn("4263 9826 4026 9299"));
-    assert!(luhn("4539 3195 0343 6467"));
-    assert!(luhn("7992 7398 713"));
-}
+    #[test]
+    #[ignore]
+    fn test_single_digit_cc_number() {
+        assert!(!luhn("0"));
+    }
 
-#[test]
-fn test_invalid_cc_number() {
-    assert!(!luhn("4223 9826 4026 9299"));
-    assert!(!luhn("4539 3195 0343 6476"));
-    assert!(!luhn("8273 1232 7352 0569"));
+    #[test]
+    #[ignore]
+    fn test_two_digit_cc_number() {
+        assert!(luhn(" 0 0 "));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_valid_cc_number() {
+        assert!(luhn("4263 9826 4026 9299"));
+        assert!(luhn("4539 3195 0343 6467"));
+        assert!(luhn("7992 7398 713"));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_invalid_cc_number() {
+        assert!(!luhn("4223 9826 4026 9299"));
+        assert!(!luhn("4539 3195 0343 6476"));
+        assert!(!luhn("8273 1232 7352 0569"));
+    }
 }
 
 use wasm_bindgen::prelude::wasm_bindgen;
